8be984fd8df4a1e7dbdf2c601e3b230930c5ce72
Mustapha-agunbiade <mustaphaagunbiade9@gmail.com>
2023-09-16 19:19:39 +0200

    i just did it

---
+++ exercise.js
@@ -0,0 +1,71 @@
+//comments makes code readable
+//Welcome to Javascript Mastery
+/* comments makes code readable, easy to reuse and informative */
+
+// 'Todoroki'
+// true
+// null
+// undefined
+
+// console.log(typeof 'Todoroki')
+// console.log(typeof true)
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+// let name 
+// let city
+// let country
+// let hobby
+
+// let firstname = 'Mustapha'
+// let lastname = 'Agunbiade'
+// let age = '12'
+// let job = 'student'
+
+// let age = '12'
+// const gravity = '9.81'
+// let mass = '45'
+// const PI = '3.14'
+// const boilingPoint = '100'
+// const bodyTemp = '37'
+// console.log(age, gravity, mass, PI, boilingPoint, bodyTemp)
+
+// const PI = Math.PI
+// console.log()
+// console.log(Math.round(PI))
+// console.log(Math.round(9.81))
+// console.log('down', Math.floor(3.5))
+// console.log(Math.ceil(PI))
+// console.log(Math.min(-5, 3, 20, 4, 5, 10))
+// console.log(Math.max(-5, 3, 20, 4, 5, 10))
+// const randNum = Math.random()
+// console.log(randNum)
+// console.log(randNum)
+// console.log(randNum)
+// console.log(randNum)
+// console.log(randNum)
+// console.log(randNum)
+
+// console.log(Math.abs(-10))
+// console.log(Math.sqrt(100))
+// console.log(Math.sqrt(2))
+// console.log(Math.pow(3,2))
+// console.log(Math.E)
+// console.log(Math.log(2))
+// console.log(Math.log(10))
+// console.log(Math.LN2)
+// console.log(Math.LN10)
+// console.log(Math.sin(0))
+// console.log(Math.sin(60))
+// console.log(Math.cos(0))
+// console.log(Math.cos(60))
+
+// console.log(Math.abs(-13))
+// console.log(Math.sqrt(625))
+// console.log(Math.pow(3,3))
+// console.log(Math.pow(4,3))
+
+let randNum = Math.random()
+let zeroTofifteen = randNum *16
+console.log(zeroTofifteen)
+console.log(Math.floor(zeroTofifteen))
